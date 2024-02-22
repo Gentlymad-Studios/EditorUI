8f2d885f15a2680ceddfcb77b8ee6215c4862ba2
Markus <markus@markuspohl.net>
2024-02-22 16:16:06 +0100

    tweak ModalInputPopup

--- Editor/UIElements/ModalInputPopup.cs
+++ Editor/UIElements/ModalInputPopup.cs
@@ -5,14 +5,91 @@ using UnityEngine.UIElements;
 
 namespace EditorUI {
     public class ModalInputPopup : EditorWindow {
+        ModalInputPopup window;
+
+        //Visual Elements
         TextField keyInput;
         Button okButton;
+
+        //Defaults
+        Vector2 windowSize = new Vector2(250, 70);
+
+        //Result
+        Result result = new Result();
+
+        //Properties
         string description;
         string okText;
         string cancelText;
-        Result result = new Result();
+        string defaultValue = string.Empty;
         Func<string, bool> inputCheck;
-        static Vector2 windowSize = new Vector2(250, 70);
+
+        public string Header {
+            set {
+                window.titleContent.text = value;
+            }
+        }
+        public string Description {
+            set {
+                description = value;
+            }
+        }
+        public string OkText {
+            set {
+                okText = value;
+            }
+        }
+        public string CancelText {
+            set {
+                cancelText = value;
+            }
+        }
+        public string DefaultValue {
+            set {
+                defaultValue = value;
+            }
+        }
+        public Func<string, bool> InputCheck {
+            set {
+                inputCheck = value;
+            }
+        }
+        public Vector2 WindowSize {
+            set {
+                window.minSize = value;
+                window.maxSize = value;
+                window.position = new Rect(EditorGUIUtility.GetMainWindowPosition().center - windowSize / 2, window.minSize);
+            }
+        }
+
+        /// <summary>
+        /// Create an Modal Input Popup
+        /// </summary>
+        /// <param name="header">Header of the Popup</param>
+        /// <param name="description">Description in the Popup</param>
+        /// <param name="okText">Text for the Ok Button</param>
+        /// <param name="cancelText">Text for the Cancel Button</param>
+        /// <param name="inputCheck">Function that should called on input change to check the typed string with</param>
+        public ModalInputPopup(string header, string description, string okText = "Ok", string cancelText = "Cancel", Func<string, bool> inputCheck = null) {
+            window = CreateInstance<ModalInputPopup>();
+            window.description = description;
+            window.okText = okText;
+            window.cancelText = cancelText;
+            window.inputCheck = inputCheck;
+            window.maxSize = windowSize;
+            window.minSize = windowSize;
+            window.titleContent = new GUIContent(header);
+            window.position = new Rect(EditorGUIUtility.GetMainWindowPosition().center - windowSize / 2, windowSize);
+        }
+
+        /// <summary>
+        /// Opens the Popup and returns the Result
+        /// </summary>
+        /// <returns></returns>
+        public Result ShowModalPopup() {
+            ShowModalUtility();
+            return result;
+        }
 
         void CreateGUI() {
             rootVisualElement.style.marginBottom = 2;
@@ -23,8 +100,8 @@ namespace EditorUI {
             Label descriptionLbl = new Label(description);
             descriptionLbl.style.marginLeft = 2;
 
-            keyInput = new TextField();
-            keyInput.RegisterValueChangedCallback(KeyInput_changed);
+            keyInput = new TextField(defaultValue);
+            keyInput.RegisterValueChangedCallback((evt) => ValidateInput(evt.newValue));
             keyInput.RegisterCallback<KeyDownEvent>(e => {
                 if (e.keyCode == KeyCode.Return) {
                     OkButton_clicked();
@@ -54,15 +131,16 @@ namespace EditorUI {
             rootVisualElement.Add(buttonContainer);
 
             keyInput.Focus();
+            ValidateInput(defaultValue);
         }
 
-        private void KeyInput_changed(ChangeEvent<string> evt) {
-            if (string.IsNullOrEmpty(evt.newValue)) {
+        private void ValidateInput(string value) {
+            if (string.IsNullOrEmpty(value)) {
                 okButton.SetEnabled(false);
                 return;
             }
 
-            if (inputCheck != null && !inputCheck.Invoke(evt.newValue)) {
+            if (inputCheck != null && !inputCheck.Invoke(value)) {
                 okButton.SetEnabled(false);
                 return;
             }
@@ -82,20 +160,6 @@ namespace EditorUI {
             Close();
         }
 
-        public static Result ShowModal(string header, string description, string okText = "Ok", string cancelText = "Cancel", Func<string, bool> inputCheck = null) {
-            ModalInputPopup window = CreateInstance<ModalInputPopup>();
-            window.description = description;
-            window.okText = okText;
-            window.cancelText = cancelText;
-            window.inputCheck = inputCheck;
-            window.maxSize = windowSize;
-            window.minSize = windowSize;
-            window.titleContent = new GUIContent(header);
-            window.position = new Rect(EditorGUIUtility.GetMainWindowPosition().center - windowSize / 2, windowSize);
-            window.ShowModalUtility();
-
-            return window.result;
-        }
     }
 
     public class Result {
